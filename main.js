4b82b77565144edaf0fa193e251c6f464b191655
gilan-star <83273499+gilan-star@users.noreply.github.com>
2021-05-15 20:38:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ const observerCallback = (entries, observer) => {
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach(section => observer.observe(section));
 
-window.addEventListener('wheel', () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY === 0) {
     selectedNavIndex = 0;
   } else if (
